d065a0cc2920e77977c2ae53a460fb7f5770f843
devingrischow <devingrischow@outlook.com>
2024-06-02 13:01:03 -0500

    Initial Commit

---
+++ ImageClassificationTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ImageClassificationTest
+//
+//  Created by Devin Grischow on 6/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ImageClassificationTest/ImageClassificationTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageClassificationTestApp.swift
+//  ImageClassificationTest
+//
+//  Created by Devin Grischow on 6/2/24.
+//
+
+import SwiftUI
+
+@main
+struct ImageClassificationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
